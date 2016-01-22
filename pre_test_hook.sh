1b670f3a226f60117b8a51c177c6f7f1ba1edf15
Doug Wiegley <doug@parksidesoftware.com>
2016-01-22 10:23:23 -0700

    experiment

--- pre_test_hook.sh
+++ pre_test_hook.sh
@@ -1,18 +1,28 @@
 #!/bin/bash -x
 
+GATE_DEST=$BASE/new
+DEVSTACK_PATH=$GATE_DEST/devstack
+
 ## A10 Software and config
 
 if [ -n "$ACOS_CLIENT_GIT" ]; then
-    sudo pip install -e "git+${ACOS_CLIENT_GIT}#egg=acos_client"
+    sudo -H pip install -e "git+${ACOS_CLIENT_GIT}#egg=acos_client"
 else
-    sudo pip install -U acos-client
+    sudo -H pip install -U acos-client
 fi
 if [ -n "$A10_NEUTRON_LBAAS_GIT" ]; then
-    sudo pip install -e "git+${A10_NEUTRON_LBAAS_GIT}#egg=a10_neutron_lbaas"
+    sudo -H pip install -e "git+${A10_NEUTRON_LBAAS_GIT}#egg=a10_neutron_lbaas"
 else
-    sudo pip install -U a10-neutron-lbaas
+    sudo -H pip install -U a10-neutron-lbaas
 fi
 set -e
+
+cat > $DEVSTACK_PATH/local.conf <<EOF
+[[post-config|\$NEUTRON_LBAAS_CONF]]
+
+[service_providers]
+service_provider=LOADBALANCER:A10Networks:neutron_lbaas.services.loadbalancer.drivers.a10networks.driver_v1.ThunderDriver:default
+EOF
 
 # Make sure we have a configuration
 
@@ -26,7 +36,7 @@ fi
 
 echo "Writing private config.py"
 sudo mkdir -p /etc/a10
-sudo chmod a+rx /etc/a10
+sudo chmod a+rwx /etc/a10
 cat - > /etc/a10/config.py <<EOF
 devices = {
     "ax1": {
