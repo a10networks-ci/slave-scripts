8c3496fb8aa2df90084edf0ff89bc7bbb754d114
Michael Durrant <mdurrant@a10networks.com>
2017-03-24 00:31:14 +0000

    Updated pretest hook to append to local.conf instead of clobber it. * Added debug statement

--- pre_test_hook.sh
+++ pre_test_hook.sh
@@ -48,16 +48,18 @@ install_python_pkg a10-openstack-lib "$A10_OPENSTACK_LIB_GIT"
 install_python_pkg a10-neutron-lbaas "$A10_NEUTRON_LBAAS_GIT"
 
 set -e
-
+# Append, don't clobber
 if [ "$testenv" != "apiv1" ]; then
-    cat > $DEVSTACK_PATH/local.conf <<EOF
+    echo "lbaasv2"
+    cat >> $DEVSTACK_PATH/local.conf <<EOF
 [[post-config|\$NEUTRON_LBAAS_CONF]]
 
 [service_providers]
 service_provider=LOADBALANCERV2:A10Networks:neutron_lbaas.drivers.a10networks.driver_v2.ThunderDriver:default
 EOF
 else
-    cat > $DEVSTACK_PATH/local.conf <<EOF
+    echo "lbaasv1"
+    cat >> $DEVSTACK_PATH/local.conf <<EOF
 [[post-config|\$NEUTRON_LBAAS_CONF]]
 
 [service_providers]
